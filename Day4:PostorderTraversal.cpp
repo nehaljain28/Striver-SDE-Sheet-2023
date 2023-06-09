90a59f51a3ec0b8c73d81499b0d5e14c048d709b
nehaljain28 <88399280+nehaljain28@users.noreply.github.com>
2023-06-09 14:55:29 +0530

    Update Day4:PostorderTraversal.cpp

--- Day4:PostorderTraversal.cpp
+++ Day4:PostorderTraversal.cpp
@@ -27,4 +27,39 @@ vector<int> getPostOrderTraversal(TreeNode *root)
     // Write your code here.
 }
 //iterative
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getPostOrderTraversal(TreeNode *curr)
+{
+    vector < int > postOrder;
+  if (curr == NULL) return postOrder;
 
+  stack < TreeNode * > s1;
+  stack < TreeNode * > s2;
+  s1.push(curr);
+  while (!s1.empty()) {
+    curr = s1.top();
+    s1.pop();
+    s2.push(curr);
+    if (curr -> left != NULL)
+      s1.push(curr -> left);
+    if (curr -> right != NULL)
+      s1.push(curr -> right);
+  }
+  while (!s2.empty()) {
+    postOrder.push_back(s2.top() -> data);
+    s2.pop();
+  }
+  return postOrder;
+}
