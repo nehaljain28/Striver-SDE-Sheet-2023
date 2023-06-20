2ba60bc155d55378f7936454b21199ddc1d64d00
nehaljain28 <88399280+nehaljain28@users.noreply.github.com>
2023-06-20 19:08:36 +0530

    Create Day10:FindMiddleElementInLinkedList.cpp

---
+++ Day10:FindMiddleElementInLinkedList.cpp
@@ -0,0 +1,40 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node *findMiddle(Node *head) {
+    if(!head)return NULL;
+    Node *fast=head;
+    Node *slow=head;
+    while(fast&&fast->next)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    return slow;
+    
+    // Write your code here
+}
+
