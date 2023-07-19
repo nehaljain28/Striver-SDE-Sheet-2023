2edf6fcd9e6e9cfb0ca285a3626cef2eff548bfa
nehaljain28 <88399280+nehaljain28@users.noreply.github.com>
2023-07-20 01:57:34 +0530

    Create Day18:TopologicalSortBFS.cpp

---
+++ Day18:TopologicalSortBFS.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>ans;
+	    queue<int>q;
+	    vector<int>indegree(V,0);
+	    for(int i=0;i<V;i++){
+	        for(int j:adj[i]){
+	            indegree[j]++;
+	        }
+	    }
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0)q.push(i);
+	    }
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(int i:adj[node]){
+	            indegree[i]--;
+	            if(indegree[i]==0)q.push(i);
+	        }
+	    }
+	    return ans;
+	    // code here
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
