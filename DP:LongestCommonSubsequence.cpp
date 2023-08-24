efd30936fcff697d0259c3c036ec744720130fcd
nehaljain28 <88399280+nehaljain28@users.noreply.github.com>
2023-08-25 00:54:47 +0530

    Create DP:LongestCommonSubsequence.cpp

---
+++ DP:LongestCommonSubsequence.cpp
@@ -0,0 +1,68 @@
+//mem
+class Solution {
+public:
+    int f(int ind1,int ind2,string text1, string text2,vector<vector<int>>&dp){
+        if(ind1<0||ind2<0)return 0;
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(text1[ind1]==text2[ind2])return 1+f(ind1-1,ind2-1,text1,text2,dp);
+        else{
+            return max(f(ind1-1,ind2,text1,text2,dp),f(ind1,ind2-1,text1,text2,dp));
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,text1,text2,dp);
+    }
+};
+
+//tab
+class Solution {
+public:
+    
+    int longestCommonSubsequence(string s1, string s2) {
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+    }
+};
+
+
+//space
+int n=s1.size();
+    int m=s2.size();
+
+    vector<int> prev(m+1,0), cur(m+1,0);
+    
+    // Base Case is covered as we have initialized the prev and cur to 0.
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                cur[ind2] = 1 + prev[ind2-1];
+            else
+                cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
+        }
+        prev= cur;
+    }
+    
+    return prev[m];
